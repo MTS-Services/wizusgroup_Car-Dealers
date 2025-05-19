ad165a8b95e03c63f167e712a284dcc4db4df5a9
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-05-19 09:27:58 +0600

    Updated

---
+++ app/Services/Admin/ProductManagement/ProductInfoCategoryService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Admin\ProductManagement;
+
+class ProductInfoCategoryService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
