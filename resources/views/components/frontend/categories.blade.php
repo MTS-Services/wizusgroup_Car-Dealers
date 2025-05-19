3cf25cb1bccee1e3d756ae43913e9761cc5da719
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-05-19 09:14:11 +0600

    catagories hover iccect addd

--- resources/views/components/frontend/categories.blade.php
+++ resources/views/components/frontend/categories.blade.php
@@ -1,10 +1,5 @@
-
-<div>
-
-    <div class="text-center">
-        <img class="w-auto rounded-xl object-cover mx-auto" src="{{ $categories['image'] }}" alt="{{ __('image') }}">
-        <p class="py-2">{{ __($categories['name']) }} </p>
-    </div>
-
-
+<div class="text-center group transition duration-300 transform hover:scale-105 hover:shadow-shadowPrimary rounded-xl overflow-hidden">
+    <img class="w-auto rounded-xl object-cover mx-auto transition duration-300 transform group-hover:scale-105 group-hover:shadow-shadowPrimary" src="{{ $categories['image'] }}" alt="{{ __('image') }}">
+    <p class="py-2 transition duration-300 group-hover:text-text.secondary">{{ __($categories['name']) }}</p>
 </div>
+
