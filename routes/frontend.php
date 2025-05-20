761b3f952f86acb584e95b16655fce1f2a45bc70
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-05-20 17:15:24 +0600

    Details page linkup done

--- routes/frontend.php
+++ routes/frontend.php
@@ -20,9 +20,9 @@ Route::group(['as' => 'frontend.'], function () {
   // Parts & Accessories Page
   Route::get('/parts-accessories', [FrontendController::class, 'parts_accessories'])->name('parts-accessories');
 //  Product Details Page
-  Route::get('/product_details', [FrontendController::class, 'product_details'])->name('product_details');
+  Route::get('/product-details', [FrontendController::class, 'product_details'])->name('product_details');
 // group Shipping page
-Route::get('/group_shipping', [FrontendController::class, 'group_shipping'])->name('group_shipping');
+Route::get('/group-shipping', [FrontendController::class, 'group_shipping'])->name('group_shipping');
 // droopshipping
 Route::get('/dropshipping', [FrontendController::class, 'dropshipping'])->name('dropshipping');
 // Regions
