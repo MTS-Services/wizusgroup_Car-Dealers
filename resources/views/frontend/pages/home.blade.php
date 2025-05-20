4aeabe64d00522fced75059fd681e51ee99338f2
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-05-20 15:43:37 +0600

    Category Dynamic On Frontend Done

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -124,7 +124,8 @@
 
 @section('content')
     {{-- ===================== banner Section Start ===================== --}}
-    <section class="lg:max-h-screen max-h-[70vh] md:max-h-[80vh] h-[calc(100vh-80px)] xs:h-[calc(100vh-60px)] relative overflow-hidden">
+    <section
+        class="lg:max-h-screen max-h-[70vh] md:max-h-[80vh] h-[calc(100vh-80px)] xs:h-[calc(100vh-60px)] relative overflow-hidden">
         <div class="absolute bg-transparent inset-0 z-10">
             <div class="container flex items-center justify-center h-full px-4 xs:px-2">
                 <div class="text-center w-full">
@@ -134,7 +135,8 @@
                     <p class="my-4 text-base xs:text-sm sm:text-lg md:text-xl text-text-white px-4 xs:px-0">
                         {{ __('Discover amazing content and features.') }}
                     </p>
-                    <div class="relative  2xl:max-w-[700px] xl:max-w-[600px] lg:max-w-[500px] max-w-96 mx-auto px-4 xs:px-2">
+                    <div
+                        class="relative  2xl:max-w-[700px] xl:max-w-[600px] lg:max-w-[500px] max-w-96 mx-auto px-4 xs:px-2">
                         <input type="search" id="machine-search"
                             class="block w-full xl:py-4 md:py-3 py-2 px-1 xs:px-2 pl-4 pr-16 text-sm xs:text-xs border-none rounded-lg bg-bg-light-secondary focus:ring-blue-500 focus:border-blue-600"
                             placeholder="{{ __('Find your machine...') }}">
@@ -162,43 +164,7 @@
     </section>
     {{-- ===================== banner Section End ===================== --}}
     {{-- ===================== Category Section Start ===================== --}}
-    @php
-        $categories = [
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here.',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
-            [
-                'image' => asset('frontend/images/tractar.jpg'),
-                'name' => 'Machine description goes here',
-            ],
 
-        ];
-    @endphp
 
     <section class="2xl:py-20 xl:py-16 lg:py-12 md:py-10 py-8">
         <div class="container">
@@ -210,7 +176,13 @@
                     <div class="swiper-wrapper">
                         @foreach ($categories as $category)
                             <div class="swiper-slide py-8">
-                                <x-frontend.category :categories="$category" />
+                                <div>
+                                    <div class="text-center">
+                                        <img class="w-auto rounded-xl object-cover mx-auto" src="{{ $category->modified_image }}"
+                                            alt="{{$category?->name }}">
+                                        <p class="py-2">{{ __($category?->name) }} </p>
+                                    </div>
+                                </div>
                             </div>
                         @endforeach
                     </div>
@@ -382,7 +354,8 @@
                     <div class="swiper-button swiper-button-next 3xl:-right-13 2xl:-right-9 ">
                         <i data-lucide="chevron-right" class="w-5 h-5 text-blue-800"></i>
                     </div>
-                    <div class=" right-10 bottom-10 z-10 fixed shadow-lg w-16 h-16 flex items-center justify-center bg-gradient-primary rounded-full">
+                    <div
+                        class=" right-10 bottom-10 z-10 fixed shadow-lg w-16 h-16 flex items-center justify-center bg-gradient-primary rounded-full">
                         <a href="#">
                             <i class="fa-brands fa-whatsapp text-5xl text-text-light"></i>
                         </a>
