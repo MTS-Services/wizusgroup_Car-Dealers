988b2eb43e43d16ec197027696b7e2bcaf0d0bc1
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-05-21 12:59:57 +0600

    Seeder add done

--- database/seeders/DocumentationSeeder.php
+++ database/seeders/DocumentationSeeder.php
@@ -14,62 +14,302 @@ class DocumentationSeeder extends Seeder
     public function run(): void
     {
        Documentation::create([
-        'title' => 'Admin Create Documentation',
+        'title' => 'Admin Create Test Documentation',
         'module_key' => 'admin',
         'type' => 'create',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Admin Update Documentation',
+        'title' => 'Admin Update Test Documentation',
         'module_key' => 'admin',
         'type' => 'update',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Permission Create Documentation',
+        'title' => 'Permission Create Test Documentation',
         'module_key' => 'permission',
         'type' => 'create',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Permission Update Documentation',
+        'title' => 'Permission Update Test Documentation',
         'module_key' => 'permission',
         'type' => 'update',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Role Create Documentation',
+        'title' => 'Role Create Test Documentation',
         'module_key' => 'role',
         'type' => 'create',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Role Update Documentation',
+        'title' => 'Role Update Test Documentation',
         'module_key' => 'role',
         'type' => 'update',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'User Create Documentation',
+        'title' => 'User Create Test Documentation',
         'module_key' => 'user',
         'type' => 'create',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'User Update Documentation',
+        'title' => 'User Update Test Documentation',
         'module_key' => 'user',
         'type' => 'update',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Supplier Create Documentation',
+        'title' => 'Supplier Create Test Documentation',
         'module_key' => 'supplier',
         'type' => 'create',
         'documentation' => 'Documentation Contents',
        ]);
        Documentation::create([
-        'title' => 'Supplier Update Documentation',
+        'title' => 'Supplier Update Test Documentation',
         'module_key' => 'supplier',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Country Create Test Documentation',
+        'module_key' => 'country',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Country Update Test Documentation',
+        'module_key' => 'country',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'State Create Test Documentation',
+        'module_key' => 'state',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'State Update Test Documentation',
+        'module_key' => 'state',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'City Create Test Documentation',
+        'module_key' => 'city',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'City Update Test Documentation',
+        'module_key' => 'city',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Operation Area Create Test Documentation',
+        'module_key' => 'operation area',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Operation Area Update Test Documentation',
+        'module_key' => 'operation area',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Operation Sub Area Create Test Documentation',
+        'module_key' => 'operation sub area',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Operation Sub Area Update Test Documentation',
+        'module_key' => 'operation sub area',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Attribute Create Test Documentation',
+        'module_key' => 'product attribute',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Attribute Update Test Documentation',
+        'module_key' => 'product attribute',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Attribute Value Create Test Documentation',
+        'module_key' => 'product attribute value',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Attribute Value Update Test Documentation',
+        'module_key' => 'product attribute value',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Company Create Test Documentation',
+        'module_key' => 'company',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Company Update Test Documentation',
+        'module_key' => 'company',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Tax class Create Test Documentation',
+        'module_key' => 'tax class',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Tax class Update Test Documentation',
+        'module_key' => 'tax class',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Tax Rate Create Test Documentation',
+        'module_key' => 'tax rate',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Tax Rate Update Test Documentation',
+        'module_key' => 'tax rate',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Brand Create Test Documentation',
+        'module_key' => 'brand',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Brand Update Test Documentation',
+        'module_key' => 'brand',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Model Create Test Documentation',
+        'module_key' => 'model',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Model Update Test Documentation',
+        'module_key' => 'model',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Category Create Test Documentation',
+        'module_key' => 'category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Category Update Test Documentation',
+        'module_key' => 'category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Sub Category Create Test Documentation',
+        'module_key' => 'sub category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Sub Category Update Test Documentation',
+        'module_key' => 'sub category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Sub Child Category Create Test Documentation',
+        'module_key' => 'sub child category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Sub Child Category Update Test Documentation',
+        'module_key' => 'sub child category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Info Category Create Test Documentation',
+        'module_key' => 'product info category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Info Category Update Test Documentation',
+        'module_key' => 'product info category',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Info Category Type Create Test Documentation',
+        'module_key' => 'product info category type',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Info Category Type Update Test Documentation',
+        'module_key' => 'product info category type',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Info Category Type Feature Create Test Documentation',
+        'module_key' => 'product info category type feature',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Product Info Category Type Feature Update Test Documentation',
+        'module_key' => 'product info category type feature',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Banner Create Test Documentation',
+        'module_key' => 'banner',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Banner Update Test Documentation',
+        'module_key' => 'banner',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Faq Create Test Documentation',
+        'module_key' => 'faq',
+        'type' => 'update',
+        'documentation' => 'Documentation Contents',
+       ]);
+       Documentation::create([
+        'title' => 'Faq Update Test Documentation',
+        'module_key' => 'faq',
         'type' => 'update',
         'documentation' => 'Documentation Contents',
        ]);
