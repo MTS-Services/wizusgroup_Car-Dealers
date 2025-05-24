b01e9276a47d8f58ba187067c3a66102ec506813
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-05-24 14:57:54 +0600

    update

---
+++ app/Http/Controllers/Frontend/PartsAccessoriesPageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class PartsAccessoriesPageController extends Controller
+{
+    public function parts(Request $request)
+    {
+        $data['products'] = Product::with(['category', 'company',  'primaryImage'])->parts()->letest()->get();
+        return view('frontend.pages.auctions', $data);
+    }
+}
