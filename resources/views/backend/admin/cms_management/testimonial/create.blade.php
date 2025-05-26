28b32d3691e77a87e8fc200eb824aa5569abaa0e
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-05-26 09:26:26 +0600

    change some layouts

--- resources/views/backend/admin/cms_management/testimonial/create.blade.php
+++ resources/views/backend/admin/cms_management/testimonial/create.blade.php
@@ -21,47 +21,55 @@
                     <form action="{{ route('cms.testimonial.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="row">
+                            <!-- Left side - Form fields -->
                             <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{ __(' Name') }} <span class="text-danger">*</span></label>
-                                    <input type="text" value="{{ old('author_name') }}" name="author_name"
-                                        class="form-control" placeholder="Enter name">
-                                    <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_name']" />
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>{{ __(' Name') }} <span class="text-danger">*</span></label>
+                                            <input type="text" value="{{ old('author_name') }}" name="author_name"
+                                                class="form-control" placeholder="Enter name">
+                                            <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_name']" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>{{ __(' Designation') }} <span class="text-danger">*</span></label>
+                                            <input type="text" value="{{ old('author_designation') }}"
+                                                name="author_designation" class="form-control"
+                                                placeholder="Enter designation">
+                                            <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_designation']" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>{{ __(' Country') }} <span class="text-danger">*</span></label>
+                                            <input type="text" value="{{ old('author_country') }}" name="author_country"
+                                                class="form-control" placeholder="Enter country">
+                                            <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_country']" />
+                                        </div>
+                                    </div>
+                                    <!-- Add other form fields here as needed -->
                                 </div>
                             </div>
+
+                            <!-- Right side - Image upload -->
                             <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{ __(' Designation') }} <span class="text-danger">*</span></label>
-                                    <input type="text" value="{{ old('author_designation') }}" name="author_designation"
-                                        class="form-control" placeholder="Enter designatione">
-                                    <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_designation']" />
+                                <div class="form-group h-100"
+                                    style="border:  padding: 20px; display: flex; flex-direction: column; justify-content: center; min-height: 100%;">
+                                    <label>{{ __('Image') }}</label>
+                                    <input type="file" name="uploadImage" data-actualName="author_image"
+                                        class="form-control filepond" id="image" accept="image/*">
+                                    <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_image']" />
                                 </div>
                             </div>
                         </div>
+
                         <div class="form-group">
-                            <label>{{ __('Image') }}</label>
-                            <input type="file" name="uploadImage" data-actualName="author_image"
-                                class="form-control filepond" id="image" accept="image/*">
-                            <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_image']" />
+                            <label>{{ __('Quote') }}</label>
+                            <textarea name="quote" class="form-control" id="quote" placeholder="Enter Quote">{{ old('quote') }}</textarea>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{ __(' Country') }} <span class="text-danger">*</span></label>
-                                    <input type="text" value="{{ old('author_country') }}" name="author_country"
-                                        class="form-control" placeholder="Enter country">
-                                    <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'author_country']" />
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{ __(' Quote') }} <span class="text-danger">*</span></label>
-                                    <input type="text" value="{{ old('quote') }}" name="quote" class="form-control"
-                                        placeholder="Enter quote">
-                                    <x-feed-back-alert :datas="['errors' => $errors, 'field' => 'quote']" />
-                                </div>
-                            </div>
-                        </div>
+
 
                         <div class="form-group float-end">
                             <input type="submit" class="btn btn-primary" value="Create">
@@ -73,6 +81,8 @@
     </div>
 @endsection
 @push('js')
+    {{-- CKEditor5 --}}
+    <script src="{{ asset('ckEditor5/main.js') }}"></script>
     {{-- FilePond  --}}
     <script src="{{ asset('filepond/filepond.js') }}"></script>
     <script>
