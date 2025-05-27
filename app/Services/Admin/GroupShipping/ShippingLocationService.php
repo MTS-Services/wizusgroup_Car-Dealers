7006878218ef19bf40da979ef156c5255084a3e4
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-05-27 11:30:37 +0600

    ShippingLocation and Container
    Model controller requset create done

---
+++ app/Services/Admin/GroupShipping/ShippingLocationService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Admin\GroupShipping;
+
+class ShippingLocationService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
