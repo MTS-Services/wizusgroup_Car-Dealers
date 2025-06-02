2b94e202d084d9b0ea0cbc9812b69f2e70c0b109
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-06-02 17:14:50 +0600

    Checkout page updated

--- app/Http/Controllers/Frontend/CheckoutPageController.php
+++ app/Http/Controllers/Frontend/CheckoutPageController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Services\AddressService;
 use App\Services\Admin\Setup\CountryService;
 use Illuminate\Http\Request;
 
 class CheckoutPageController extends Controller
 {
     protected CountryService $countryService;
+    protected AddressService $addressService;
 
-    public function __construct(CountryService $countryService)
+    public function __construct(CountryService $countryService, AddressService $addressService)
     {
         $this->countryService = $countryService;
+        $this->addressService = $addressService;
     }
     public function checkout()
     {
