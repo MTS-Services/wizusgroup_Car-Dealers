d816451bae19d489eb4e84714c954b5af5126410
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-06-04 16:41:45 +0600

    user iamge prblm issue fix

--- app/Http/Requests/User/UserProfileRequest.php
+++ app/Http/Requests/User/UserProfileRequest.php
@@ -28,7 +28,7 @@ class UserProfileRequest extends FormRequest
             'username' => 'nullable|string|min:3',
             // 'email' => 'required|email|unique:users,email,',
             'phone' => 'nullable|string|size:11',
-            'image'=> 'nullable',
+            'image'=> 'nullable|image|mimes:jpg,jpeg,png,webp,svg|max:2048',
             'gender' => 'nullable',
             'dob' => 'nullable|date',
             'father_name' => 'nullable|string|min:3',
