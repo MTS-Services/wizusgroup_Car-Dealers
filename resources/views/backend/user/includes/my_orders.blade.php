c0ac30e2c371a2b312ee2f29aeafc4d1660d1387
Md Shariful Islam <devshariful99@gmail.com>
2025-06-10 12:36:41 +0600

    Updated

--- resources/views/backend/user/includes/my_orders.blade.php
+++ resources/views/backend/user/includes/my_orders.blade.php
@@ -86,20 +86,29 @@
                                 <td class="px-6 py-4 whitespace-nowrap text-center">
                                     @if ($order->status == App\Models\Order::STATUS_INITIATED)
                                         <a href="{{ route('frontend.checkout', $order->order_number) }}"
+                                            title="Checkout"
                                             class="inline-block text-text-secondary hover:text-text-tertiary mr-3">
                                             <i data-lucide="eye" class="w-5 h-5"></i>
                                         </a>
                                     @elseif ($order->status == App\Models\Order::STATUS_PENDING)
                                         <a href="{{ route('frontend.container-order', $order->order_number) }}"
+                                            title="Container Order"
                                             class="inline-block text-text-secondary hover:text-text-tertiary mr-3">
                                             <i data-lucide="eye" class="w-5 h-5"></i>
                                         </a>
                                     @else
                                         <a href="{{ route('user.order.details', $order->order_number) }}"
+                                            title="Details"
                                             class="inline-block text-text-secondary hover:text-text-tertiary mr-3">
                                             <i data-lucide="eye" class="w-5 h-5"></i>
                                         </a>
+                                        <a href="{{ route('user.container.details', $order->container?->slug) }}"
+                                            title="Container"
+                                            class="inline-block text-text-secondary hover:text-text-tertiary mr-3">
+                                            <i data-lucide="arrow-right" class="w-5 h-5"></i>
+                                        </a>
                                     @endif
+
 
                                 </td>
                             </tr>
