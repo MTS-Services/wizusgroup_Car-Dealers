e7b39e919f740ff0b7739667b9fec92942565542
Md Shariful Islam <devshariful99@gmail.com>
2025-06-19 13:50:16 +0600

    Updated

--- resources/js/select2.js
+++ resources/js/select2.js
@@ -2,9 +2,12 @@ import Select2 from "select2";
 import "select2/dist/css/select2.min.css";
 window.Select2 = Select2;
 
-document.addEventListener("DOMContentLoaded", () => {
-    const selects = document.querySelectorAll("select");
-    selects.forEach((select) => {
-        new Select2(select);
+document.addEventListener("DOMContentLoaded", function () {
+    const selects = document.querySelectorAll("select.form-control:not(.no-select)");
+    selects.forEach(select => {
+        $(select).select2({
+            tags: true,
+            tokenSeparators: [',']
+        });
     });
 });
